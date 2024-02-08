97a96df919ded9eb2dbadb2e6dd828242bfcc689
Karel Tuček <kareltucek@centrum.cz>
2024-02-08 01:27:51 +0100

    Move node specific things to node_specific.ts

--- src/uhk_preset.ts
+++ src/uhk_preset.ts
@@ -100,17 +100,3 @@ export function startUhkCli() {
         Cli.launch(builder);
     })
 }
-
-if (
-    typeof process !== undefined &&
-    process.versions != null &&
-    process.versions.node != null
-) {
-    const args = process.argv.slice(2);
-
-    if (args[0] && args[0] == 'start') {
-        startUhkCli();
-    } else if (args[0] && args[0] != '') {
-        console.log("You can start the cli with `start` argument. E.g., `node file.js start`.")
-    }
-}
